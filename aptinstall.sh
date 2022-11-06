4b0245ee36d8bd9e83888027992358f6f583920f
Roman Weintraub <roman.weintraub@googlemail.com>
2022-11-06 03:11:37 +0100

    only execute sudo is the command is installed

--- aptinstall.sh
+++ aptinstall.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:lazygit-team/release
+# check if sudo is available 
+export sudo
+if [ -x "$(command -v sudo)" ]; then
+  sudo="sudo" 
+else 
+  sudo=""
+fi
 
-sudo apt update && sudo apt full-upgrade -y
+eval $sudo add-apt-repository -y ppa:lazygit-team/release
+
+eval $sudo apt update && eval $sudo apt full-upgrade -y
 
 function install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    sudo apt install -y $1
+    eval $sudo apt install -y $1
   else
     echo "Already installed: ${1}"
   fi
@@ -20,8 +28,8 @@ function bin-install {
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    sudo curl -L "$2" -o "/usr/local/bin/$1"
-    sudo chmod +x "/usr/local/bin/$1"
+    eval $sudo curl -L "$2" -o "/usr/local/bin/$1"
+    eval $sudo chmod +x "/usr/local/bin/$1"
   else
     echo "Already installed: ${1}"
   fi
@@ -32,8 +40,8 @@ function tar-install {
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    curl -L "$2" | sudo tar xfvz - -C "/usr/local/bin" --strip-components=1 --wildcards "*/$1"
-    sudo chmod +x "/usr/local/bin/$1"
+    curl -L "$2" | eval $sudo tar xfvz - -C "/usr/local/bin" --strip-components=1 --wildcards "*/$1"
+    eval $sudo chmod +x "/usr/local/bin/$1"
   else
     echo "Already installed: ${1}"
   fi
@@ -63,6 +71,15 @@ if [ $? -ne 0 ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else
   echo "Already installed: Homebrew"
+fi
+
+# wsl helper
+which wslview &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Installing: wsl utilities"
+  curl -sL https://raw.githubusercontent.com/wslutilities/wslu/master/extras/scripts/wslu-install | bash
+else
+  echo "Already installed: wsl utilities"
 fi
 
 # cli ux
